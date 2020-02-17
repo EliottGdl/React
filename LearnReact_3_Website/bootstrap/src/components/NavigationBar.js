bc0d79763b79923c72575406231e6024d50bb736
Eliott Gandiolle <eliottgdl@gmail.com>
2020-02-17 23:08:27 +0100

    petit changement

--- LearnReact_3_Website/bootstrap/src/components/NavigationBar.js
+++ LearnReact_3_Website/bootstrap/src/components/NavigationBar.js
@@ -19,14 +19,13 @@ const Styles = styled.div`
 export const NavigationBar = () => (
     <Styles>
         <Navbar expand="lg">
-            <Navbar.Brand href="/"> Eliott Gandiolle </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Brand href="/"> Eliott Gandiolle </Navbar.Brand>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto"> 
                     <Nav.Item> <Nav.Link href="/"> Home </Nav.Link> </Nav.Item>
                     <Nav.Item> <Nav.Link href="/about"> About </Nav.Link> </Nav.Item>
                     <Nav.Item> <Nav.Link href="/contact"> Contact </Nav.Link> </Nav.Item>
-
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
