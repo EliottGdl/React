b78f84b613ff7944e7c78eeab9853b62e7e765fe
Eliott Gandiolle <eliottgdl@gmail.com>
2020-02-18 22:23:30 +0100

    adding most recent tracks / cookie when staying connected

--- LearnReact_3_Website/bootstrap/src/About.js
+++ LearnReact_3_Website/bootstrap/src/About.js
@@ -3,10 +3,7 @@ import SpotifyAuth from "./components/SpotifyAuth"
 
 export const About = () => (
     <div>
-        <h2> About Page </h2>
-        <p> aze pojazej azope jp azpodjazopd zkapod paz dopkaz </p>
-        <p> azoiej aiojzd poazk dkzapapz odapzok ddz </p>
-        <p> azeazeaze aiojzd poazk dkzapapz odapzok ddz </p>
+        <h2> Get my most recent listening </h2>
         <SpotifyAuth/>
     </div>
 )
--- LearnReact_3_Website/bootstrap/src/components/SpotifyAuth.js
+++ LearnReact_3_Website/bootstrap/src/components/SpotifyAuth.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { id } from "./conf";
 import logo from "../assets/iconSpotify.png";
+import $ from "jquery";
 
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 const clientId = id;
 const redirectUri = "http://localhost:3000/about";
-const scopes = ["user-read-currently-playing", "user-read-playback-state"];
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+  "user-read-recently-played"
+];
 
 const hash = window.location.hash
   .substring(1)
@@ -22,32 +27,80 @@ const hash = window.location.hash
 window.location.hash = "";
 
 export default class SpotifyAuth extends Component {
-  
   state = {
-      token: null
+    token:
+      null ||
+      document.cookie.replace(
+        /(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/,
+        "$1"
+      )
+  };
+
+  getWhatIlistend(tok) {
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player/recently-played",
+      type: "GET",
+      beforeSend: xhr => {
+        xhr.setRequestHeader("Authorization", "Bearer " + tok);
+      },
+      success: data => {
+        let eAll = "";
+        let ecoutes = [];
+        for (let item of data.items) {
+          eAll += item.track.name + " Artists : ";
+          for (let artiste of item.track.artists) {
+            eAll += artiste.name + " ";
+          }
+          ecoutes.push(eAll);
+          eAll = "";
+        }
+
+        this.setState({
+          token: tok,
+          songs: ecoutes
+        });
+      }
+    });
   }
 
   componentDidMount() {
-      console.log(hash.access_token)
-    let _token = hash.access_token;
-    if (_token) {
-      this.setState({
-        token: _token
-      });
+    if (!this.state.token) {
+      let _token = hash.access_token;
+      if (_token) {
+        document.cookie = "token=" + _token;
+        this.getWhatIlistend(_token);
+        this.setState({
+          token: _token
+        });
+      }
+    } else {
+      this.getWhatIlistend(this.state.token);
     }
   }
 
   render() {
     return (
       <div>
-        {this.state.token ? (<p> Vous etes connectes </p>) : (<a
-          className="btn btn--loginApp-link"
-          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-            "%20"
-          )}&response_type=token&show_dialog=true`}
-        >
-          <img src={logo} className="App-logo" alt="logo" />
-        </a>)}
+        {this.state.token ? (
+          this.state.songs ? (
+            this.state.songs.map((item, index) => (
+              <p key = {index}>
+                {index + 1} / {item}{" "}
+              </p>
+            ))
+          ) : (
+            <p> Chargement</p>
+          )
+        ) : (
+          <a
+            className="btn btn--loginApp-link"
+            href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+              "%20"
+            )}&response_type=token&show_dialog=true`}
+          >
+            <img src={logo} className="App-logo" alt="logo" />
+          </a>
+        )}
       </div>
     );
   }
