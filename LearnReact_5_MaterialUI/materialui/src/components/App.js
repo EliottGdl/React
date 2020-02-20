8fc3da00a47039ac18614ccf88aa8f23327d0934
Eliott Gandiolle <eliottgdl@gmail.com>
2020-02-21 00:25:43 +0100

    discover new list/objects tips

--- LearnReact_5_MaterialUI/materialui/src/components/App.js
+++ LearnReact_5_MaterialUI/materialui/src/components/App.js
@@ -1,14 +1,39 @@
 import React, { Component, Fragment } from "react";
 import { Header, Footer } from "./Layouts";
 import Exercises from "./Exercises";
+import {muscles,exercises} from "../store.js";
+
 
 export default class extends Component {
+
+  state = {
+    exercises 
+  }
+
+  getExercisesByMuscles() {
+    return this.state.exercises.reduce((exercises,exercise) => {
+      const {muscles} = exercise
+
+      console.log(exercises[muscles])
+      exercises[muscles] = exercises[muscles] ? [...exercises[muscles],exercises] : [exercise]
+
+      return exercises
+    }, {})
+  }
+
   render() {
+    
+    this.getExercisesByMuscles()
+
     return (
       <Fragment>
         <Header />
-        <Exercises />
-        <Footer />
+        <Exercises 
+          exercises = {exercises}
+        />
+        <Footer 
+          muscles = {muscles}
+        />
       </Fragment>
     );
   }
--- LearnReact_5_MaterialUI/materialui/src/components/Layouts/Footer.js
+++ LearnReact_5_MaterialUI/materialui/src/components/Layouts/Footer.js
@@ -2,17 +2,20 @@ import React from "react";
 import {Paper,Tabs} from "@material-ui/core"
 import Tab from '@material-ui/core/Tab';
 
-export default props => (
+export default ({muscles}) => (
   <Paper>
   <Tabs
     value={0}
     indicatorColor="primary"
     textColor="primary"
     centered
-  >
-    <Tab label="Item One" />
-    <Tab label="Item Two" />
-    <Tab label="Item Three" />
+  > 
+  <Tab label="All"/>
+
+  {muscles.map(grp => 
+      <Tab label={grp} />
+  )}
+
   </Tabs>
 </Paper>
 );
