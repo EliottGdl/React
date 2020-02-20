ecb27a8d87d99e651b05934804014a7038674108
Eliott Gandiolle <eliottgdl@gmail.com>
2020-02-20 15:09:45 +0100

    header/footer

--- LearnReact_5_MaterialUI/materialui/src/components/Layouts/Header.js
+++ LearnReact_5_MaterialUI/materialui/src/components/Layouts/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 
 export default props => (
   <AppBar position="static">
-    <Toolbar />
+    <Toolbar>
+      <Typography variant="h3" color="inherit">
+        Exercise
+      </Typography>
+    </Toolbar>
   </AppBar>
 );
