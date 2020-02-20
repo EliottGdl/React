cd4fb9883abbd744338bf99380fedc9a16693f79
Eliott Gandiolle <eliottgdl@gmail.com>
2020-02-20 15:25:07 +0100

    styles from gss / dividing in 2 different panes

--- LearnReact_5_MaterialUI/materialui/src/components/Exercises/index.js
+++ LearnReact_5_MaterialUI/materialui/src/components/Exercises/index.js
@@ -1,3 +1,18 @@
 import React from "react";
+import {Grid} from "@material-ui/core"
+import LeftPane from "./LeftPane";
+import RightPane from "./RightPane"
 
-export default props => <div>Hello from Exercises</div>;
+const styles = {
+    Paper : {padding:20,marginTop:10,marginBottom:10}
+}
+
+export default props => 
+    <Grid container sm={12}>
+        <Grid item sm={6}>
+            <LeftPane styles={styles}/>
+        </Grid>
+        <Grid item sm>
+            <RightPane styles={styles}/>
+        </Grid>
+    </Grid>
