98c3ae604e6bf45704ea6289e02d6b13fcf1d6d7
Eliott Gandiolle <eliottgdl@gmail.com>
2020-02-22 15:01:09 +0100

    first form

--- LearnReact_5_MaterialUI/materialui/src/components/Exercises/Dialogs/Create.js
+++ LearnReact_5_MaterialUI/materialui/src/components/Exercises/Dialogs/Create.js
@@ -11,42 +11,62 @@ import AddIcon from "@material-ui/icons/Add";
 import Box from "@material-ui/core/Box";
 
 export default class extends Component {
-
-    state = {
-        open:false
+  state = {
+    open: false,
+    exercise : {
+        title:"",
+        description:"",
+        muscles:""
     }
+  };
 
-    handleToggle = () => {
-        this.setState({
-            open : !this.state.open,
-        })
-    }
+  handleToggle = () => {
+    this.setState({
+      open: !this.state.open
+    });
+  };
+
+  handleChange = name => ({target:{value}}) => {
+      this.setState({
+          exercise: {
+            ...this.state.exercise,
+            [name]:value,
+          }
+      })
+  };
 
   render() {
-
-    const {open} = this.state;
+    const { open , exercise:{title,description,muscles} } = this.state;
 
     return (
       <Fragment>
         <Box borderRadius="50%">
-          <Button aria-label="add" variant="contained" onClick={this.handleToggle}>
+          <Button
+            aria-label="add"
+            variant="contained"
+            onClick={this.handleToggle}
+          >
             <AddIcon />
           </Button>
         </Box>
 
-        <Dialog
-          open={open}
-          onClose={this.handleToggle}
-        >
+        <Dialog open={open} onClose={this.handleToggle}>
           <DialogTitle id="form-dialog-title">Create an exercise</DialogTitle>
           <DialogContent>
             <DialogContentText>
               Please fill out the form below
             </DialogContentText>
-            <form></form>
+            <form>
+              <TextField
+                label="Title"
+                onChange = {this.handleChange('title')}
+                value={title}
+                margin="normal"
+              />
+            </form>
           </DialogContent>
           <DialogActions>
-            <Button color="primary" variant = "contained">
+            <Button color="primary" variant="contained">
               Create
             </Button>
           </DialogActions>
