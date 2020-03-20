3c43de95c29ea48684a68b52e501f46bffcacc77
Eliott Gandiolle <eliottgdl@gmail.com>
2020-03-20 22:00:31 +0100

    styles

--- LearnReact_7_ReactNative/MesFilms/Components/Search.js
+++ LearnReact_7_ReactNative/MesFilms/Components/Search.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
-import {View,Button,TextInput} from 'react-native';
+import {View,Button,TextInput,StyleSheet} from 'react-native';
 
 export default class Search extends Component {
     render() {
         return (
-            <View>
-                <TextInput placeholder="Titre du film"></TextInput>
-                <Button title="Rechercher" onPress={()=>{}}> </Button>
+            <View style={styles.vue}>
+                <TextInput style={[styles.inputs,styles.spaceAllAround]} placeholder="Titre du film"></TextInput>
+                {/** Buttons can't have any style, you need to create a TouchableNativeFeedback */}
+                <Button title="Rechercher" onPress={()=>{}}/>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    vue: {
+      marginTop : 20,
+    },
+
+    spaceAllAround: {
+        marginLeft:10,
+        marginRight:10,
+        marginBottom:10,
+        marginTop:10
+    },
+
+    inputs: {
+        
+        height:50,
+        borderColor: '#000000',
+        borderWidth:1,
+        paddingLeft:5,
+        
+    },
+
+});
+  
