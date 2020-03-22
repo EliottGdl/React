6b1a83de692b0ba9e827715702c69d0a0295190e
Eliott Gandiolle <eliottgdl@gmail.com>
2020-03-22 12:56:28 +0100

    few changes

--- LearnReact_7_ReactNative/MesFilms/Api/TDMApi.js
+++ LearnReact_7_ReactNative/MesFilms/Api/TDMApi.js
@@ -6,3 +6,10 @@ export function getFilmsFromApiWithSearchedText(text,page) {
         .then((response) => response.json())
         .catch((error) => console.log(error))
 }
+
+export function getFilmDetailFromApi(id) {
+    const url = 'https://api.themoviedb.org/3/search/movie'+id+'?api_key=' + token + '&language=fr';
+    return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.log(error))
+}
--- LearnReact_7_ReactNative/MesFilms/Components/FilmDetail.js
+++ LearnReact_7_ReactNative/MesFilms/Components/FilmDetail.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
-import {StyleSheet,View,Text} from 'react-native';
+import {StyleSheet,View,Text,ActivityIndicator} from 'react-native';
 
 export default class FilmDetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.setState={
+            film:undefined,
+            isLoading:true,
+        }
+    }
+
+    _displayLoading() {
+        if(this.state.isLoading) {
+            return (<View style={styles.loading}>
+                <ActivityIndicator size="large"/>
+            </View>)
+        }
+    }
+
     render() {
         const idFilm = this.props.navigation.state.params.idFilm;
         return (
             <View style={styles.main_container}>
                 <Text> Détail du film </Text>
+                {this._displayLoading()}
             </View>
         )
     }
@@ -15,5 +33,16 @@ export default class FilmDetail extends Component {
 const styles = StyleSheet.create({
     main_container: {
         flex:1,
-    }
+    },
+
+    loading:{
+        position:"absolute",
+        left:0,
+        right:0,
+        top:0,
+        bottom:0,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+
 })
