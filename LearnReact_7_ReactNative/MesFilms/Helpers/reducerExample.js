5f40892fb107da8cffb00df5cd85b4ae8a1a46eb
Eliott Gandiolle <eliottgdl@gmail.com>
2020-03-23 22:23:26 +0100

    reducer

---
+++ LearnReact_7_ReactNative/MesFilms/Helpers/reducerExample.js
@@ -0,0 +1,16 @@
+function reducerProfil(state,action) {
+    let nextState
+
+    switch(action.type) {
+        case 'ADD_PROFIL':
+            return nextState;
+        case 'UPDATE_PROFIL':
+            return nextState
+        case 'DELETE_PROFIL':
+            return nextState
+        default:
+            return nextState
+    }
+
+    return nextState
+}
