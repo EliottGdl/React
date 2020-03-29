1632e794116db5370de30ce81c28ec7f745bf9dc
Eliott Gandiolle <eliottgdl@gmail.com>
2020-03-29 22:02:21 +0200

    react native full native project

---
+++ LearnReact_7_ReactNative/MesFilms/Components/Avatar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Avatar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            avatar : require("../Images/ic_tag_faces.png");
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
